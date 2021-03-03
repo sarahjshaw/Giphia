1e8e93e58faf02f110748f49e684b168f338a2d5
mhrecin <mhrecin@gmail.com>
2021-03-03 18:54:20 -0500

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { CreateProfileComponent } from './create-profile/create-profile.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    SignInComponent,
+    CreateProfileComponent,
+    LeaderboardComponent
   ],
   imports: [
     BrowserModule,
