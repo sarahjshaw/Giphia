5d1d0089713f498f1d163705e7fc01ec3a44cc41
Christa Wilson <chrwilso14@yahoo.com>
2021-03-17 18:51:05 -0400

    default-player-name

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,86 +13,21 @@ import { ChooseAvatarComponent } from './choose-avatar/choose-avatar.component';
 
 export const routes: Routes = [
 
-  { path: 'home', component: HomeComponent, 
-     data: { show:true,
-             showChildren:false,
-             navLabel:'Home',
-             title:'Giphia',
-             description: 'trivia game with a giphy twist',
-             icon:'house',
-           }
-  },
+  { path: 'home', component: HomeComponent },
 
-  { path: 'leaderboard', component: LeaderboardComponent,
-     data: { show:true,
-             showChildren:false,
-             navLabel:'LeaderBoard',
-             title:'Giphia',
-             description: 'trivia game with a giphy twist',
-             icon:'trophy',
-            }
-  },
+  { path: 'leaderboard', component: LeaderboardComponent },
 
-  { path: 'signin', component: SignInComponent,
-     data: { show:true,
-             showChildren:false,
-             navLabel:'Sign In',
-             title:'Giphia',
-             description: 'trivia game with a giphy twist',
-             icon:'person',
-           }
-  },
+  { path: 'signin', component: SignInComponent },
 
-  { path: 'chooseavatar', component: ChooseAvatarComponent,
-     data: { show:true,
-          showChildren:false,
-          navLabel:'Choose Avatar',
-          title:'Giphia',
-          description: 'trivia game with a giphy twist', 
-          icon: 'person',
-        }
-},
+  { path: 'chooseavatar', component: ChooseAvatarComponent },
 
-  { path: 'player-profile', component: PlayerProfileComponent,
-     data: { show:true,
-             showChildren:false,
-             navLabel:'Profile',
-             title:'Giphia',
-             description: 'trivia game with a giphy twist',
-             icon: 'person',
-            }
-   },
+  { path: 'player-profile', component: PlayerProfileComponent },
 
-  { path: 'gamemodes', component: GameModesComponent,
-     data: { show:true,
-             showChildren:false,
-             navLabel:'Game Modes',
-             title:'Giphia',
-             description: 'trivia game with a giphy twist',
-             icon: 'game controller',
-            }
-  },
+  { path: 'gamemodes', component: GameModesComponent },
 
-  { path: 'endless-trivia', component: EndlessTriviaComponent,
-     data: { show:true,
-             showChildren:false,
-             navLabel:'Endless Trivia',
-             title:'Giphia',
-             description: 'trivia game with a giphy twist', 
-             icon: 'question mark',
-   }
-  },
+  { path: 'endless-trivia', component: EndlessTriviaComponent },
 
-  { path: 'time-challenge', component: TimeChallengeComponent,
-     data: { show:true,
-     showChildren:false,
-     navLabel:'Time Challenge',
-     title:'Giphia',
-     description: 'trivia game with a giphy twist',
-     icon: 'stop watch',
-     
-     }
-  },
+  { path: 'time-challenge', component: TimeChallengeComponent },
 
   { path: '', redirectTo: '/home', pathMatch: 'full'},
  
